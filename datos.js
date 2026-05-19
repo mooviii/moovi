b88ae5694af647b638d4bb476eae105d03ee8153
mooviii <vitolfritol@gmail.com>
2026-05-19 21:59:17 +0100

    Add files via upload

---
+++ datos.js
@@ -0,0 +1,27 @@
+const videoDB = {
+
+"101": { src: "video1.mp4", title: "E.T. El extraterrestre", year: "1982", director: "Steven Spielberg" },
+"102": { src: "video2.mp4", title: "El padrino", year: "1972", director: "Francis Ford Coppola" },
+"103": { src: "video3.mp4", title: "Sin Perdón", year: "1992", director: "Clint Eastwood" },
+"104": { src: "video4.mp4", title: "El silencio de los corderos", year: "1991", director: "Jonathan Demme" },
+"105": { src: "video5.mp4", title: "Indiana Jones y el templo maldito", year: "1984", director: "Steven Spielberg" },
+"106": { src: "video6.mp4", title: "La lista de Schindler", year: "1993", director: "Steven Spielberg" },
+"107": { src: "video7.mp4", title: "Cadena perpétua", year: "1994", director: "Frank Darabont" },
+"108": { src: "video8.mp4", title: "Pulp fiction", year: "1994", director: "Quentin Tarantino" },
+"109": { src: "video9.mp4", title: "La vida es bella", year: "1997", director: "Roberto Benigni" },
+"110": { src: "video10.mp4", title: "Seven", year: "1995", director: "David Fincher" },
+"111": { src: "video11.mp4", title: "Forrest Gump", year: "1994", director: "Robert Zemeckis" },
+"112": { src: "video12.mp4", title: "En el nombre del padre", year: "1993", director: "Jim Sheridan" },
+"113": { src: "video13.mp4", title: "Reservoir Dogs", year: "1992", director: "Quentin Tarantino" },
+"114": { src: "video14.mp4", title: "El club de la lucha", year: "1999", director: "David Fincher" },
+"115": { src: "video15.mp4", title: "El apartamento", year: "1960", director: "Billy Wilder" },
+"116": { src: "video16.mp4", title: "La gran evasión", year: "1963", director: "John Sturges" },
+"117": { src: "video17.mp4", title: "El bueno, el feo y el malo", year: "1966", director: "Sergio Leone" },
+"118": { src: "video18.mp4", title: "Doctor Zhivago", year: "1965", director: "David Lean" },
+"119": { src: "video19.mp4", title: "Espartaco", year: "1960", director: "Stanley Kubrick" },
+"120": { src: "video20.mp4", title: "La dolce vita", year: "1960", director: "Federico Fellini" },
+"121": { src: "video21.mp4", title: "2001, Una odisea en el espacio", year: "1968", director: "Stanley Kubrick" },
+"122": { src: "video22.mp4", title: "Cowboy de medianoche", year: "1969", director: "John Schlesinger" },
+
+
+};
